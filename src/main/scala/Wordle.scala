f208fbd8d7150e163e7a07959f48976091dc0d4a
Tom Kuson <12143269+tjkuson@users.noreply.github.com>
2023-04-12 11:17:15 +0100

    Use recursion instead of a while loop

--- src/main/scala/Wordle.scala
+++ src/main/scala/Wordle.scala
@@ -1,42 +1,50 @@
 package me.tjkuson.wordle
 
-object Wordle:
-  def main(args: Array[String]): Unit =
-    val word = getRandomWord
-    var guessesRemaining = 5
-    while true do
-      if guessesRemaining == 0 then
-        println(s"You ran out of guesses! The word was $word")
-        return
-      println(s"You have $guessesRemaining guesses remaining")
-      val guess = scala.io.StdIn.readLine("Guess the word: ").toUpperCase()
-      guess match
-        case g if g == word =>
-          println("You guessed the word!")
-          return
-        case g if g.length != word.length =>
-          println("The word is not the right length")
-        case _ =>
-          // Print the word where the user had the right letter in the right place, with the wrong letters as underscores
-          println(
-            word
-              .zip(guess)
-              .map((letter, guess) => if letter == guess then letter else '_')
-              .mkString
-          )
-          // Get list of letters that are in the wrong place but are in the word
-          val correctLettersNotInPlace = word
-            .zip(guess)
-            .filter((letter, guess) => letter != guess && word.contains(guess))
-            .map((*, guess) => guess)
-          // Print the letters that are in the word but in the wrong place
-          if correctLettersNotInPlace.nonEmpty then
-            println(
-              s"Letters in the word but in the wrong place: ${correctLettersNotInPlace.mkString(", ")}"
-            )
-          guessesRemaining -= 1
+import scala.annotation.tailrec
 
-  private def getRandomWord =
+object Wordle:
+
+  def main(args: Array[String]): Unit =
+    val guessesRemaining = 5
+    playGame(getRandomWord, guessesRemaining)
+
+  @tailrec
+  private def playGame(word: String, guessesRemaining: Int): Unit =
+    guessesRemaining match
+      case 0 =>
+        println("You lose!")
+      case _ =>
+        println(s"Guesses remaining: $guessesRemaining")
+        val guess = scala.io.StdIn.readLine("Guess a word: ").toUpperCase
+        guess match
+          case `word` =>
+            println("You win!")
+          case _ if guess.length != word.length =>
+            println("Word length does not match")
+            playGame(word, guessesRemaining)
+          case _ =>
+            // Print the word where the user had the right letter in the right place, with the wrong letters as underscores
+            println(
+              word
+                .zip(guess)
+                .map((letter, guess) => if letter == guess then letter else '_')
+                .mkString
+            )
+            // Get list of letters that are in the wrong place but are in the word
+            val correctLettersNotInPlace = word
+              .zip(guess)
+              .filter((letter, guess) =>
+                letter != guess && word.contains(guess)
+              )
+              .map((_, guess) => guess)
+            // Print the letters that are in the word but in the wrong place
+            if correctLettersNotInPlace.nonEmpty then
+              println(
+                s"Letters in the word but in the wrong place: ${correctLettersNotInPlace.mkString(", ")}"
+              )
+            playGame(word, guessesRemaining - 1)
+
+  private def getRandomWord: String =
     // TODO: Get words from a file
     val words = List(
       "Hello",
